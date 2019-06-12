27bb26e483a419541b925f1552fb87c061c078ff
maciejkaczorowski <maciej.kaczorowski@strix.net>
2019-06-12 14:19:14 +0200

    feature(templateDesigner): #17 Presenting / Interactive grid layer had been implemented.

---
+++ components/TemplateGrid/TemplateDesigner/TemplateGridDesigner.vue
@@ -0,0 +1,64 @@
+/*
+* Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+* See LICENSE for license details.
+*/
+<template>
+    <div class="template-grid">
+        <slot />
+    </div>
+</template>
+
+<script>
+import debounce from 'debounce';
+
+export default {
+    name: 'TemplateGridDesigner',
+    props: {
+        maxRow: {
+            type: Number,
+            required: false,
+            default: 0,
+        },
+    },
+    data() {
+        return {
+            rowHeight: 50,
+        };
+    },
+    mounted() {
+        this.emitRowsNumber();
+        window.addEventListener('resize', this.debounceRowsNumberCalculation);
+    },
+    destroyed() {
+        window.removeEventListener('resize', this.debounceRowsNumberCalculation);
+    },
+    methods: {
+        debounceRowsNumberCalculation(time = 300) {
+            debounce(() => {
+                this.emitRowsNumber();
+            }, time)();
+        },
+        emitRowsNumber() {
+            this.$emit('rowsCount', { key: 'rowsCount', value: this.getTotalRowsInTemplate() });
+        },
+        getTotalRowsInTemplate() {
+            const { clientHeight } = this.$el;
+            const visibleRows = Math.ceil(clientHeight / this.rowHeight);
+
+            return Math.max(this.maxRow, visibleRows) + 1;
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+    .template-grid {
+        position: relative;
+        display: flex;
+        flex: 1;
+        flex-direction: column;
+        width: 0;
+        margin: 12px;
+        overflow: auto;
+    }
+</style>
