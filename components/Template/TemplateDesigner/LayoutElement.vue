3b0f0fed7eb52d5cf38dd950450c7f8582cbc2ba
maciejkaczorowski <maciej.kaczorowski@strix.net>
2019-07-02 18:00:31 +0200

    feature(templateDesigner): #17 Added shadow on hover to layout element

--- components/Template/TemplateDesigner/LayoutElement.vue
+++ components/Template/TemplateDesigner/LayoutElement.vue
@@ -311,6 +311,14 @@ export default {
         user-select: none;
         cursor: grab;
 
+        &:hover {
+            border: unset;
+            box-shadow:
+                0 2px 2px 0 rgba(0, 0, 0, 0.14),
+                0 3px 1px -2px rgba(0, 0, 0, 0.12),
+                0 1px 5px 0 rgba(0, 0, 0, 0.2);
+        }
+
         &__resizer {
             position: absolute;
             bottom: 0;
