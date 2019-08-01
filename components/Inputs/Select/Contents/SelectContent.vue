c046935187ea59c9cca478090e85f9ea6b99d0bf
maciejkaczorowski <maciej.kaczorowski@strix.net>
2019-08-01 08:17:38 +0200

    feature(uiComponents) #104 Clean code

--- components/Inputs/Select/Contents/SelectContent.vue
+++ components/Inputs/Select/Contents/SelectContent.vue
@@ -71,7 +71,6 @@ export default {
             0 2px 2px 0 rgba(0, 0, 0, 0.14),
             0 3px 1px -2px rgba(0, 0, 0, 0.12),
             0 1px 5px 0 rgba(0, 0, 0, 0.2);
-        min-width: 130px;
 
         &--fixed {
             max-height: 200px;
