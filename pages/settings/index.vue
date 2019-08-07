d69c81efd4a0205f5246be440cb8df2b25f4e45b
maciejkaczorowski <maciej.kaczorowski@strix.net>
2019-08-07 12:30:58 +0200

    feature(uiComponents) #119 Created Slider / RangeSlider component

--- pages/settings/index.vue
+++ pages/settings/index.vue
@@ -3,16 +3,32 @@
  * See LICENSE for license details.
  */
 <template>
-    <PageWrapper />
+    <PageWrapper>
+        <Slider v-model="value" />
+        <RangeSlider v-model="rangeValue" />
+    </PageWrapper>
 </template>
 
 <script>
 import PageWrapper from '~/components/Layout/PageWrapper';
+import Slider from '~/components/Inputs/Slider/Slider';
+import RangeSlider from '~/components/Inputs/Slider/RangeSlider';
 
 export default {
     name: 'Settings',
     components: {
         PageWrapper,
+        Slider,
+        RangeSlider,
+    },
+    data() {
+        return {
+            value: 0,
+            rangeValue: {
+                to: 0,
+                from: 0,
+            },
+        };
     },
 };
 </script>
