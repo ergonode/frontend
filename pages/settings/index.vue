eb00deb62ff1bf89b1fe7c18a84f90ebd8e569ec
maciejkaczorowski <maciej.kaczorowski@strix.net>
2019-08-07 12:33:21 +0200

    feature(uiComponents) #119 Clean code

--- pages/settings/index.vue
+++ pages/settings/index.vue
@@ -3,32 +3,16 @@
  * See LICENSE for license details.
  */
 <template>
-    <PageWrapper>
-        <Slider v-model="value" />
-        <RangeSlider v-model="rangeValue" />
-    </PageWrapper>
+    <PageWrapper />
 </template>
 
 <script>
 import PageWrapper from '~/components/Layout/PageWrapper';
-import Slider from '~/components/Inputs/Slider/Slider';
-import RangeSlider from '~/components/Inputs/Slider/RangeSlider';
 
 export default {
     name: 'Settings',
     components: {
         PageWrapper,
-        Slider,
-        RangeSlider,
-    },
-    data() {
-        return {
-            value: 0,
-            rangeValue: {
-                to: 0,
-                from: 0,
-            },
-        };
     },
 };
 </script>
