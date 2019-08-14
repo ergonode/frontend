3faa1658f72f8c000941f6448bcbb8719a942e67
maciejkaczorowski <maciej.kaczorowski@strix.net>
2019-08-14 16:58:03 +0200

    feature(uiComponents) #124 Created ColorPicker component with context ratio calculations

--- pages/settings/index.vue
+++ pages/settings/index.vue
@@ -3,16 +3,30 @@
  * See LICENSE for license details.
  */
 <template>
-    <PageWrapper />
+    <PageWrapper>
+        <div>
+            <ColorPicker
+                regular
+                solid
+                v-model="value"/>
+        </div>
+    </PageWrapper>
 </template>
 
 <script>
 import PageWrapper from '~/components/Layout/PageWrapper';
+import ColorPicker from '~/components/Inputs/Color/ColorPicker';
 
 export default {
     name: 'Settings',
     components: {
         PageWrapper,
+        ColorPicker,
+    },
+    data() {
+        return {
+            value: null,
+        };
     },
 };
 </script>
