2658e2679bf2d02737be34eb9370887e82576dc3
Piotr Bletek <piotr.bletek@bold.net.pl>
2019-08-21 13:42:00 +0200

    bug(workflow) ERG-#136 privileges on dashboard

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -7,7 +7,7 @@
         <SimpleGrid
             class="grid"
             :title="title"
-            :items="items" />
+            :items="filteredItems" />
     </div>
 </template>
 
@@ -23,8 +23,12 @@ export default {
     data() {
         return {
             title: 'Take an action',
-            items: GridItems,
         };
+    },
+    computed: {
+        filteredItems() {
+            return GridItems.filter(e => this.$canIUse(e.permission));
+        },
     },
 };
 </script>
