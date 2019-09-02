5d8c55df6988c9a695c0ad0f7af18530f2ff1309
Piotr Bletek <piotr.bletek@bold.net.pl>
2019-09-02 08:38:04 +0200

    ERG-#143 clean code

--- components/Card/CategoryTreeBaseTab.vue
+++ components/Card/CategoryTreeBaseTab.vue
@@ -3,12 +3,6 @@
  * See LICENSE for license details.
  */
 <template>
-    <!-- <div class="tab-wrapper">
-        <ListSearchHeader
-            header="Categories tree"
-            @searchResults="onSearch" />
-        <CategoriesList :language-code="userLanguageCode" />
-    </div> -->
     <BaseTab>
         <CategoryTreeBaseCard slot="content" />
         <Footer
@@ -35,33 +29,4 @@ export default {
         },
     },
 };
-// import { mapState } from 'vuex';
-
-// export default {
-//     name: 'CategoryTreeBaseTab',
-//     components: {
-//         CategoriesList: () => import('~/components/List/CategoriesList'),
-//         ListSearchHeader: () => import('~/components/List/ListSearchHeader'),
-//     },
-//     computed: {
-//         ...mapState('authentication', {
-//             userLanguageCode: state => state.user.language,
-//         }),
-//     },
-//     methods: {
-//         onSearch() {
-//             // TODO: Add whenever we handle it at the BE
-//         },
-//     },
-// };
 </script>
-
-<style lang="scss" scoped>
-    // .tab-wrapper {
-    //     position: relative;
-    //     display: flex;
-    //     flex: 1;
-    //     flex-direction: column;
-    //     width: 275px;
-    // }
-</style>
