85f7aa44943b1b74c0fe42d168129d6233e6f1c2
maciejkaczorowski <maciej.kaczorowski@strix.net>
2019-09-26 21:24:20 +0200

    bugfix(release) firefox - drag and drop - remove column - trash can

--- components/DragAndDrop/TrashCan.vue
+++ components/DragAndDrop/TrashCan.vue
@@ -37,7 +37,13 @@ export default {
             'setDraggableState',
         ]),
         drop(event) {
-            event.dataTransfer.clearData();
+            event.preventDefault();
+
+            if (navigator.userAgent.toLowerCase().indexOf('firefox') === -1) {
+                // TODO: Only Firefox Error: Modifications are not allowed for this document
+                // check why firefox does not support clearData
+                event.dataTransfer.clearData();
+            }
         },
         dragOver(event) {
             event.preventDefault();
