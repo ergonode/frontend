655bc9178eab73ad5ef63bd84fb94fe8b7da56d0
maciejkaczorowski <maciej.kaczorowski@strix.net>
2019-12-24 01:24:58 +0100

    bugfix(release) refactor

---
+++ core/components/Buttons/Fab.vue
@@ -0,0 +1,61 @@
+/*
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE for license details.
+ */
+<template>
+    <Button
+        v-bind="$attrs"
+        fab
+        @mouseenter.native="onMouseEnter"
+        @mouseleave.native="onMouseLeave">
+        <template #append>
+            <slot
+                name="icon"
+                :fill-color="iconFillColor"
+                :is-hovered="isHovered" />
+        </template>
+    </Button>
+</template>
+<script>
+import { THEMES } from '~/defaults/buttons';
+import {
+    GREEN, GRAPHITE, WHITE, GREY_DARK,
+} from '~/assets/scss/_variables/_colors.scss';
+import Button from '~/core/components/Buttons/Button';
+
+export default {
+    name: 'Fab',
+    inheritAttrs: false,
+    components: {
+        Button,
+    },
+    data() {
+        return {
+            isHovered: false,
+        };
+    },
+    computed: {
+        iconFillColor() {
+            if (this.$attrs.theme === THEMES.SECONDARY) {
+                if (this.$attrs.disabled) {
+                    return GREY_DARK;
+                }
+                if (this.isHovered) {
+                    return GREEN;
+                }
+                return GRAPHITE;
+            }
+
+            return WHITE;
+        },
+    },
+    methods: {
+        onMouseEnter() {
+            this.isHovered = true;
+        },
+        onMouseLeave() {
+            this.isHovered = false;
+        },
+    },
+};
+</script>
