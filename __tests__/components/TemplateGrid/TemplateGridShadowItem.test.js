af438873738e60ecaa4d1c30b916d4c93ad8d9fe
maciejkaczorowski <maciej.kaczorowski@strix.net>
2020-01-03 09:07:52 +0100

    feature(grid) test

---
+++ __tests__/components/TemplateGrid/TemplateGridShadowItem.test.js
@@ -0,0 +1,45 @@
+/*
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE for license details.
+ */
+import { shallowMount } from '@vue/test-utils';
+import TemplateGridShadowItem from '~/components/TemplateGrid/TemplateGridShadowItem';
+
+describe('TemplateGrid/TemplateGridShadowItem', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallowMount(TemplateGridShadowItem, {
+            propsData: {
+                lastRow: false,
+                itemId: 1,
+            },
+        });
+    });
+
+    it('Component is named well', () => {
+        expect(typeof TemplateGridShadowItem.name).toBe('string');
+        expect(TemplateGridShadowItem.name).toEqual('TemplateGridShadowItem');
+    });
+    it('Check if not last row', () => {
+        expect(wrapper.contains('.shadow-grid-item--last-row')).toBeFalsy();
+    });
+    it('Check if last row', () => {
+        wrapper.setProps({
+            lastRow: true,
+        });
+        expect(wrapper.contains('.shadow-grid-item--last-row')).toBeTruthy();
+    });
+    it('Check item default id', () => {
+        expect(wrapper.vm.lastRow).toBeFalsy();
+        expect(wrapper.vm.itemId).toBe(1);
+        expect(wrapper.contains('[shadow-id="0"]')).toBe(true);
+    });
+    it('Check item id', () => {
+        wrapper.setProps({
+            itemId: 3,
+        });
+        expect(wrapper.vm.lastRow).toBeFalsy();
+        expect(wrapper.vm.itemId).toBe(3);
+        expect(wrapper.contains('[shadow-id="2"]')).toBe(true);
+    });
+});
