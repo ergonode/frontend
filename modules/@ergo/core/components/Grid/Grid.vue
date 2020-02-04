d58d53ec2bb71e0131f4dbe8cd1e82be4f1fb7ce
maciejkaczorowski <maciej.kaczorowski@strix.net>
2020-02-04 10:52:00 +0100

    feature(configuration) clean code

--- modules/@ergo/core/components/Grid/Grid.vue
+++ modules/@ergo/core/components/Grid/Grid.vue
@@ -2,10 +2,6 @@
  * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
  * See LICENSE for license details.
  */
-/*
-* Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
-* See LICENSE for license details.
-*/
 <script>
 import { mapState, mapActions } from 'vuex';
 import {
