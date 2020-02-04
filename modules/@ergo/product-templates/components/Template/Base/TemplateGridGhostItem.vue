7e5766ee305e8cd71a8ffecc820c2c30e3f9580c
maciejkaczorowski <maciej.kaczorowski@strix.net>
2020-02-04 10:37:54 +0100

    feature(configuration) resolving conflicts

--- modules/@ergo/product-templates/components/Template/Base/TemplateGridGhostItem.vue
+++ modules/@ergo/product-templates/components/Template/Base/TemplateGridGhostItem.vue
@@ -3,145 +3,56 @@
  * See LICENSE for license details.
  */
 <template>
-    <div
-        :class="['ghost-item', highlightedStateClasses]"
-        @dragenter="onDragEnter"
-        @dragleave="onDragLeave"
-        @dragover="onDragOver"
-        @drop="onDrop" />
+    <div class="template-grid-ghost-item">
+        <div class="container">
+            <IconAddFilter :fill-color="whiteColor" />
+            ADD ATTRIBUTE
+        </div>
+    </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
 import {
-    addGhostElementToDraggableLayer,
-    removeGhostElementFromDraggableLayer,
-} from '@Templates/models/layout/GhostElement';
+    WHITE,
+} from '@Core/assets/scss/_js-variables/colors.scss';
 
 export default {
     name: 'TemplateGridGhostItem',
-    props: {
-        position: {
-            type: Object,
-            required: true,
-        },
-        highlightingPositions: {
-            type: Array,
-            default: () => [],
-        },
-    },
-    data() {
-        return {
-            isGhostElement: false,
-        };
+    components: {
+        IconAddFilter: () => import('~/modules/@ergo/core/components/Icons/Actions/IconAddFilter.vue'),
     },
     computed: {
-        ...mapState('draggable', {
-            draggedElement: state => state.draggedElement,
-        }),
-        highlightedStateClasses() {
-            return {
-                'ghost-item--highlighted': this.highlightedElement,
-                'ghost-item--top-border': this.highlightedElement && !this.isTopNeighbour,
-                'ghost-item--right-border': this.highlightedElement && !this.isRightNeighbour,
-            };
-        },
-        highlightedElement() {
-            return this.highlightingPositions.find(this.isEqualToPosition);
-        },
-        isTopNeighbour() {
-            const { row: rowPos, column: columnPos } = this.position;
-
-            return this.highlightingPositions.some(
-                ({ row, column }) => row === rowPos - 1 && column === columnPos,
-            );
-        },
-        isRightNeighbour() {
-            const { row: rowPos, column: columnPos } = this.position;
-
-            return this.highlightingPositions.some(
-                ({ row, column }) => row === rowPos && column === columnPos + 1,
-            );
-        },
-    },
-    methods: {
-        onDrop(event) {
-            event.preventDefault();
-            if (this.isGhostElement) {
-                removeGhostElementFromDraggableLayer();
-
-                this.$emit('drop', this.position);
-            }
-        },
-        onDragOver(event) {
-            event.preventDefault();
-        },
-        onDragEnter() {
-            if (this.highlightedElement && !this.highlightedElement.isObstacle) {
-                this.addGhostElement();
-                this.isGhostElement = true;
-            }
-        },
-        onDragLeave() {
-            if (this.highlightedElement && !this.highlightedElement.isObstacle) {
-                removeGhostElementFromDraggableLayer();
-                this.isGhostElement = false;
-            }
-        },
-        isEqualToPosition(position) {
-            const { row, column } = this.position;
-
-            return row === position.row && column === position.column;
-        },
-        addGhostElement() {
-            let draggedElementWidth = 1;
-            let draggedElementHeight = 1;
-
-            if (typeof this.draggedElement === 'object') {
-                draggedElementWidth = this.draggedElement.width;
-                draggedElementHeight = this.draggedElement.height;
-            }
-
-            const elementsGap = 16;
-            const {
-                width, height,
-            } = this.$el.getBoundingClientRect();
-            const normalizedWidth = width * draggedElementWidth - elementsGap;
-            const normalizedHeight = height * draggedElementHeight - elementsGap;
-
-            addGhostElementToDraggableLayer({
-                top: this.$el.offsetTop + (elementsGap / 2),
-                left: this.$el.offsetLeft + (elementsGap / 2),
-                width: normalizedWidth,
-                height: normalizedHeight,
-            });
+        whiteColor() {
+            return WHITE;
         },
     },
 };
 </script>
 
 <style lang="scss" scoped>
-    .ghost-item {
-        position: relative;
+    .template-grid-ghost-item {
+        $filter: &;
+
+        position: fixed;
+        z-index: $Z_INDEX_LVL_1;
         display: flex;
-        padding: 8px;
-        transition: all 0.3s cubic-bezier(0.25, 0.8, 0.5, 1);
-        border-left: $BORDER_DASHED_GREY;
-        border-bottom: $BORDER_DASHED_GREY;
+        flex: 1;
+        flex-direction: column;
+        align-items: flex-start;
+        padding: 12px;
+        box-sizing: border-box;
+        pointer-events: none;
+        background-color: $GREEN;
+        box-shadow: $ELEVATOR_HOLE;
+    }
 
-        &--highlighted {
-            flex: 1;
-            background-color: $GREEN_LIGHT;
-            border-left: $BORDER_DASHED_GREEN;
-            border-bottom: $BORDER_DASHED_GREEN;
-        }
-
-        &--top-border {
-            border-top: $BORDER_DASHED_GREEN;
-        }
-
-        &--right-border {
-            border-right: $BORDER_DASHED_GREEN;
-        }
+    .container {
+        display: grid;
+        grid-column-gap: 8px;
+        grid-auto-flow: column;
+        grid-template-columns: max-content;
+        align-items: center;
+        color: $WHITE;
+        font: $FONT_BOLD_12_16;
     }
 </style>
