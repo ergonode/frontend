57df3c84e1a6df59f6db5d892c202583b7845c6b
Maciej Kaczorowski <46050724+derpdead@users.noreply.github.com>
2020-02-14 07:33:17 +0100

    Erg form validations (#302)
    * feature(configuration) resolving conflicts
    
    * feature(architecture) improve error validations

--- modules/@ergo/core/store/validations/mutations.js
+++ modules/@ergo/core/store/validations/mutations.js
@@ -5,22 +5,14 @@
 import defaultState from './state';
 
 export const types = {
-    SET_ERROR_FOR_KEY: 'SET_ERROR_FOR_KEY',
-    SET_VALIDATION_ERRORS: 'SET_VALIDATION_ERRORS',
-    APPEND_VALIDATION_ERROR: 'APPEND_VALIDATION_ERROR',
+    SET_ERRORS: 'SET_ERRORS',
     REMOVE_VALIDATION_ERROR: 'REMOVE_VALIDATION_ERROR',
     REMOVE_VALIDATION_ERRORS: 'REMOVE_VALIDATION_ERRORS',
     CLEAR_STATE: 'CLEAR_STATE',
 };
 export default {
-    [types.SET_ERROR_FOR_KEY](state, { key, error }) {
-        state.validationErrors = { ...state.validationErrors, [key]: error };
-    },
-    [types.SET_VALIDATION_ERRORS](state, errorMessage) {
-        state.validationErrors = errorMessage;
-    },
-    [types.APPEND_VALIDATION_ERROR](state, errorMessage) {
-        state.validationErrors = { ...state.validationErrors, ...errorMessage };
+    [types.SET_ERRORS](state, errors) {
+        state.validationErrors = { ...state.validationErrors, ...errors };
     },
     [types.REMOVE_VALIDATION_ERROR](state, errorKey) {
         delete state.validationErrors[errorKey];
