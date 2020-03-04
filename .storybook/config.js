c4890ca7efccf8b93c5bce605c600ee6c56986ee
maciejkaczorowski <maciej.kaczorowski@strix.net>
2020-03-04 08:39:46 +0100

    feature(storybook) setup config

---
+++ .storybook/config.js
@@ -0,0 +1,7 @@
+/*
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE for license details.
+ */
+module.exports = {
+    stories: ['../stories/**/*.stories.[tj]s'],
+};
