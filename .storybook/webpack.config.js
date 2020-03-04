4f2175090677ac1d9b1adfc7ad22a5da6039f375
maciejkaczorowski <maciej.kaczorowski@strix.net>
2020-03-04 08:42:08 +0100

    feature(collections) refactor

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,3 +2,21 @@
  * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
  * See LICENSE for license details.
  */
+const path = require("path");
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.s?css$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader'],
+                include: path.resolve(__dirname, '../')
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            '@': path.dirname(path.resolve(__dirname))
+        }
+    }
+};
