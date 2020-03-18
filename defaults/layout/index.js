37e662ec22d5f2052bc17de648a63df9d63efd3b
maciejkaczorowski <maciej.kaczorowski@strix.net>
2020-03-18 20:04:35 +0100

    test

--- defaults/layout/index.js
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
- * See LICENSE for license details.
- */
-export const LayoutOrientation = {
-    HORIZONTAL: 'horizontal-layout',
-    VERTICAL: 'vertical-layout',
-};
--- defaults/operators/index.js
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
- * See LICENSE for license details.
- */
-export const FILTER_OPERATOR = {
-    GREATER_OR_EQUAL: '>=',
-    SMALLER_OR_EQUAL: '<=',
-    EQUAL: '=',
-};
