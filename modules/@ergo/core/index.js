370365f4eb63608d16b3f34005cbacb095f90dc1
Piotr Bletek <p.bletek@gmail.com>
2020-03-20 10:14:22 +0100

    perf(modularity): new VueMS features: css, plugin, store installed by library

--- modules/@ergo/core/index.js
+++ modules/@ergo/core/index.js
@@ -2,38 +2,21 @@
  * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
  * See LICENSE for license details.
  */
-import { resolve, join } from 'path';
+import { join } from 'path';
 import configuration from './config';
 
-export default async function () {
+export default async function ({ allModules }) {
     const {
-        name = '',
-        type,
-        plugins = [],
-        css = [],
+        name,
         styleResources = {},
     } = configuration;
-    const moduleName = name.replace(/[^a-zA-Z]/g, '');
-    const modulePath = type === 'npm'
-        ? `~/vendor/${name}/src`
-        : `~/modules/${name}`;
 
     if (!this.options.styleResources) this.options.styleResources = {};
-    if (!this.options.ergoModules) this.options.ergoModules = {};
 
-    this.options.ergoModules[name] = type;
-
-    css.forEach((style) => {
-        this.options.css.push(join(modulePath, style));
-    });
     Object.keys(styleResources).forEach((resource) => {
-        this.options.styleResources[resource] = join(modulePath, styleResources[resource]);
-    });
-    plugins.forEach(({ ssr, src }) => {
-        this.addPlugin({
-            src: resolve(__dirname, `${src}.js`),
-            fileName: join('modules', moduleName, `${src}.js`),
-            ssr,
-        });
+        this.options.styleResources[resource] = join(
+            allModules.find(m => m.name === name).path,
+            styleResources[resource],
+        ).replace(/\/$/g, '');
     });
 }
