9c34d22822f9082921ab94a4f118f0ea2419c7dc
maciejkaczorowski <maciej.kaczorowski@strix.net>
2020-04-06 12:58:39 +0200

    bugfix(release) clean code

--- modules/@ergo/core/components/Grid/GridFilterCell.vue
+++ modules/@ergo/core/components/Grid/GridFilterCell.vue
@@ -101,7 +101,6 @@ export default {
                 return '';
             }
 
-
             if (this.isSelectKind) {
                 return getMappedGridColumnOptions(
                     this.filter.value,
