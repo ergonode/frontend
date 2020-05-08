3b9524d93396ca5ce05fa5c690e2f43ec27224f0
maciejkaczorowski <maciej.kaczorowski@strix.net>
2020-05-08 12:06:35 +0200

    feature(product) #384 add group tab

--- modules/@ergo/products/components/Tabs/ProductGroupTab.vue
+++ modules/@ergo/products/components/Tabs/ProductGroupTab.vue
@@ -3,18 +3,99 @@
  * See LICENSE for license details.
  */
 <template>
-    <ResponsiveCenteredViewTemplate :fixed="true">
-        // TODO: Waiting for design
+    <ResponsiveCenteredViewTemplate>
+        <template #content>
+            <Grid
+                :is-editable="$hasAccess(['PRODUCT_PRODUCT_UPDATE'])"
+                :columns="columns"
+                :data-count="filtered"
+                :data="data"
+                :is-header-visible="true"
+                :is-centered-view="true"
+                :is-basic-filter="true"
+                @removeRow="onRemoveRow"
+                @fetchData="getGridData">
+                <template #actions>
+                    <ActionButton
+                        title="ADD PRODUCTS"
+                        :theme="secondaryTheme"
+                        :disabled="!isUserAllowedToUpdate"
+                        :size="smallSize"
+                        :options="addProductOptions"
+                        :fixed-content="true"
+                        @input="onSelectAddProductOption">
+                        <template #prepend="{ color }">
+                            <IconAdd :fill-color="color" />
+                        </template>
+                    </ActionButton>
+                </template>
+            </Grid>
+            <Component
+                v-if="selectedAppModalOption"
+                :is="modalComponent"
+                @close="onCloseModal"
+                @added="onCreatedData" />
+        </template>
     </ResponsiveCenteredViewTemplate>
 </template>
 
 <script>
+import { SIZE, THEME } from '@Core/defaults/theme';
+import { ADD_PRODUCT } from '@Products/defaults/add-product';
 import ResponsiveCenteredViewTemplate from '@Core/components/Layout/Templates/ResponsiveCenteredViewTemplate';
+import fetchGridDataMixin from '@Core/mixins/grid/fetchGridDataMixin';
+import IconAdd from '@Core/components/Icons/Actions/IconAdd';
+import ActionButton from '@Core/components/Buttons/ActionButton';
 
 export default {
     name: 'ProductGroupTab',
     components: {
         ResponsiveCenteredViewTemplate,
+        ActionButton,
+        IconAdd,
+    },
+    mixins: [fetchGridDataMixin({ path: 'products/_id/history' })],
+    data() {
+        return {
+            selectedAppModalOption: null,
+        };
+    },
+    computed: {
+        isUserAllowedToUpdate() {
+            return this.$hasAccess(['PRODUCT_UPDATE']);
+        },
+        smallSize() {
+            return SIZE.SMALL;
+        },
+        secondaryTheme() {
+            return THEME.SECONDARY;
+        },
+        addProductOptions() {
+            return Object.values(ADD_PRODUCT);
+        },
+        modalComponent() {
+            switch (this.selectedAppModalOption) {
+            case ADD_PRODUCT.FROM_LIST:
+                return null;
+            case ADD_PRODUCT.FROM_SEGMENT:
+                return () => import('@Collections/components/Modals/AddProductsFromSegmentModalForm');
+            case ADD_PRODUCT.BY_SKU:
+                return () => import('@Collections/components/Modals/AddProductsBySKUModalForm');
+            default: return null;
+            }
+        },
+    },
+    methods: {
+        onSelectAddProductOption(option) {
+            this.selectedAppModalOption = option;
+        },
+        onCloseModal() {
+            this.selectedAppModalOption = null;
+        },
+        onCreatedData() {
+            this.getGridData(this.localParams);
+            this.selectedAppModalOption = null;
+        },
     },
 };
 </script>
