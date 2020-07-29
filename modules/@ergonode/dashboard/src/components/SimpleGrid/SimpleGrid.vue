444b16d6164bd28367f04e2433dee2e33486c9d1
Maciej Kaczorowski <kaczorowski46@gmail.com>
2020-07-29 11:18:40 +0200

    rebase dashboard

---
+++ modules/@ergonode/dashboard/src/components/SimpleGrid/SimpleGrid.vue
@@ -0,0 +1,89 @@
+/*
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE for license details.
+ */
+/*
+* Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+* See LICENSE for license details.
+*/
+<template>
+    <div class="grid-container">
+        <span
+            class="grid-title"
+            v-text="title" />
+        <div
+            v-for="item in items"
+            :key="item.title"
+            class="grid-item">
+            <div class="item-wrapper">
+                <img
+                    :src="require(`@Dashboard/assets/images/${item.icon}`)"
+                    :alt="`${item.title} icon`">
+                <span
+                    class="item-title"
+                    v-text="item.title" />
+                <Button
+                    :title="item.actionTitle "
+                    @click.native="$router.push(item.action)" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SimpleGrid',
+    components: {
+        Button: () => import('@Core/components/Buttons/Button'),
+    },
+    props: {
+        items: {
+            type: Array,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+    .grid-container {
+        display: grid;
+        grid-template-columns: repeat(3, 251px);
+        grid-gap: 49px;
+        padding-bottom: 86px;
+
+        .grid-title {
+            height: 37px;
+            color: $GRAPHITE_DARK;
+            font: $FONT_MEDIUM_24_32;
+            text-align: center;
+            grid-column: 1/4;
+            grid-row: 1;
+        }
+
+        .grid-item {
+            height: 251px;
+            border: $BORDER_1_GREY;
+            padding: 20px;
+            background-color: $WHITE;
+
+            .item-wrapper {
+                display: grid;
+                height: 100%;
+                justify-items: center;
+                grid-gap: 20px;
+
+                .item-title {
+                    height: 24px;
+                    color: $GRAPHITE_DARK;
+                    font: $FONT_MEDIUM_24_32;
+                    text-align: center;
+                }
+            }
+        }
+    }
+</style>
