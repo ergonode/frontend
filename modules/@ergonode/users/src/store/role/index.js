aae345825341b4af02282a221fe874a0c60b6b1f
derpdead <kaczorowski46@gmail.com>
2020-09-07 12:59:26 +0200

    feature(core): clean code

---
+++ modules/@ergonode/users/src/store/role/index.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE for license details.
+ */
+import actions from './actions';
+import state from './state';
+
+export default {
+    namespaced: true,
+    state,
+    actions,
+};
