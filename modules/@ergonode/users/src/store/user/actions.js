ef5fb8fe8a5a97a2258defa595a079524b1addbf
Piotr Bletek <p.bletek@gmail.com>
2020-10-27 09:26:22 +0100

    refactor(user): extend user vuex actions

--- modules/@ergonode/users/src/store/user/actions.js
+++ modules/@ergonode/users/src/store/user/actions.js
@@ -21,77 +21,99 @@ export default {
         },
         {
             id,
+            onError = () => {},
         },
     ) {
-        const getActiveLanguageByCode = rootGetters['core/getActiveLanguageByCode'];
-        const {
-            email = '',
-            avatar_filename = '',
-            first_name = '',
-            last_name = '',
-            language = '',
-            password = '',
-            password_repeat = '',
-            is_active = false,
-            role_id,
-            language_privileges_collection = null,
-        } = await get({
-            $axios: this.app.$axios,
-            id,
-        });
+        try {
+            // EXTENDED BEFORE METHOD
+            await this.$extendMethods('@Users/store/user/action/getUser/__before', {
+                $this: this,
+                data: {
+                    id,
+                },
+            });
+            // EXTENDED BEFORE METHOD
 
-        commit('__SET_STATE', {
-            key: 'id',
-            value: id,
-        });
-        commit('__SET_STATE', {
-            key: 'avatarId',
-            value: avatar_filename ? avatar_filename.split('.')[0] : '',
-        });
-        commit('__SET_STATE', {
-            key: 'email',
-            value: email,
-        });
-        commit('__SET_STATE', {
-            key: 'firstName',
-            value: first_name,
-        });
-        commit('__SET_STATE', {
-            key: 'lastName',
-            value: last_name,
-        });
-        commit('__SET_STATE', {
-            key: 'language',
-            value: getActiveLanguageByCode(language).name,
-        });
-        commit('__SET_STATE', {
-            key: 'languageCode',
-            value: language,
-        });
-        commit('__SET_STATE', {
-            key: 'password',
-            value: password,
-        });
-        commit('__SET_STATE', {
-            key: 'passwordRepeat',
-            value: password_repeat,
-        });
-        commit('__SET_STATE', {
-            key: 'isActive',
-            value: is_active,
-        });
-        commit('__SET_STATE', {
-            key: 'languagePrivilegesCollection',
-            value: isObject(language_privileges_collection)
-                ? {
-                    ...language_privileges_collection,
-                }
-                : language_privileges_collection,
-        });
-        commit('__SET_STATE', {
-            key: 'role',
-            value: role_id,
-        });
+            const getActiveLanguageByCode = rootGetters['core/getActiveLanguageByCode'];
+            const data = await get({
+                $axios: this.app.$axios,
+                id,
+            });
+            const {
+                email = '',
+                avatar_filename = '',
+                first_name = '',
+                last_name = '',
+                language = '',
+                password = '',
+                password_repeat = '',
+                is_active = false,
+                role_id,
+                language_privileges_collection = null,
+            } = data;
+
+            commit('__SET_STATE', {
+                key: 'id',
+                value: id,
+            });
+            commit('__SET_STATE', {
+                key: 'avatarId',
+                value: avatar_filename ? avatar_filename.split('.')[0] : '',
+            });
+            commit('__SET_STATE', {
+                key: 'email',
+                value: email,
+            });
+            commit('__SET_STATE', {
+                key: 'firstName',
+                value: first_name,
+            });
+            commit('__SET_STATE', {
+                key: 'lastName',
+                value: last_name,
+            });
+            commit('__SET_STATE', {
+                key: 'language',
+                value: getActiveLanguageByCode(language).name,
+            });
+            commit('__SET_STATE', {
+                key: 'languageCode',
+                value: language,
+            });
+            commit('__SET_STATE', {
+                key: 'password',
+                value: password,
+            });
+            commit('__SET_STATE', {
+                key: 'passwordRepeat',
+                value: password_repeat,
+            });
+            commit('__SET_STATE', {
+                key: 'isActive',
+                value: is_active,
+            });
+            commit('__SET_STATE', {
+                key: 'languagePrivilegesCollection',
+                value: isObject(language_privileges_collection)
+                    ? {
+                        ...language_privileges_collection,
+                    }
+                    : language_privileges_collection,
+            });
+            commit('__SET_STATE', {
+                key: 'role',
+                value: role_id,
+            });
+
+            // EXTENDED AFTER METHOD
+            await this.$extendMethods('@Users/store/user/action/getUser/__after', {
+                $this: this,
+                data,
+            });
+            // EXTENDED AFTER METHOD
+        } catch (e) {
+            onError(e);
+        }
     },
     async updateUser(
         {
@@ -147,7 +169,7 @@ export default {
                 mappedDrafts[languageCode][privilege] = Boolean(drafts[key]);
             });
 
-            const data = {
+            let data = {
                 firstName,
                 lastName,
                 language: getActiveLanguageByName(language).code,
@@ -160,6 +182,21 @@ export default {
                     mappedDrafts,
                 ),
             };
+            // EXTENDED BEFORE METHOD
+            const extendedData = await this.$extendMethods('@Users/store/user/action/updateUser/__before', {
+                $this: this,
+                data: {
+                    id,
+                    ...data,
+                },
+            });
+            extendedData.forEach((extend) => {
+                data = {
+                    ...data,
+                    ...extend,
+                };
+            });
+            // EXTENDED BEFORE METHOD
 
             await update({
                 $axios: this.app.$axios,
@@ -177,6 +214,13 @@ export default {
                     root: true,
                 });
             }
+
+            // EXTENDED AFTER METHOD
+            await this.$extendMethods('@Users/store/user/action/updateUser/__after', {
+                $this: this,
+                data,
+            });
+            // EXTENDED AFTER METHOD
 
             onSuccess();
         } catch (e) {
@@ -209,7 +253,7 @@ export default {
                 language,
             } = state;
             const getActiveLanguageByName = rootGetters['core/getActiveLanguageByName'];
-            const data = {
+            let data = {
                 email,
                 firstName,
                 lastName,
@@ -220,12 +264,35 @@ export default {
                 isActive,
             };
 
+            // EXTENDED BEFORE METHOD
+            const extendedData = await this.$extendMethods('@Users/store/user/action/createUser/__before', {
+                $this: this,
+                data,
+            });
+            extendedData.forEach((extended) => {
+                data = {
+                    ...data,
+                    ...extended,
+                };
+            });
+            // EXTENDED BEFORE METHOD
+
             const {
                 id,
             } = await create({
                 $axios: this.app.$axios,
                 data,
             });
+
+            // EXTENDED AFTER METHOD
+            await this.$extendMethods('@Users/store/user/action/createUser/__after', {
+                $this: this,
+                data: {
+                    id,
+                    ...data,
+                },
+            });
+            // EXTENDED AFTER METHOD
 
             onSuccess(id);
         } catch (e) {
@@ -244,15 +311,30 @@ export default {
             onError = () => {},
         },
     ) {
-        const {
-            id,
-        } = state;
-
         try {
+            const {
+                id,
+            } = state;
+
+            // EXTENDED BEFORE METHOD
+            await this.$extendMethods('@Users/store/user/action/createUser/__before', {
+                $this: this,
+                data: {
+                    id,
+                },
+            });
+            // EXTENDED BEFORE METHOD
+
             await remove({
                 $axios: this.app.$axios,
                 id,
             });
+
+            // EXTENDED AFTER METHOD
+            await this.$extendMethods('@Users/store/user/action/createUser/__after', {
+                $this: this,
+            });
+            // EXTENDED AFTER METHOD
 
             onSuccess();
         } catch (e) {
