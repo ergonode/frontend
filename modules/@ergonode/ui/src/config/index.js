bb5282655f030533ea25e6270bd16cc811a953ae
derpdead <kaczorowski46@gmail.com>
2020-10-27 14:15:05 +0100

    feature(ui): init module

---
+++ modules/@ergonode/ui/src/config/index.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE for license details.
+ */
+export default {
+    name: '@ergonode/ui',
+    order: 30,
+    aliases: {
+        '@UI': '/',
+    },
+    plugins: [],
+};
