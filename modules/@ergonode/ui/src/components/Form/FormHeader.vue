f0062bb22760ea2df3f251faf748e550b0b54a62
Piotr Bletek <p.bletek@gmail.com>
2021-01-14 09:41:52 +0100

    Feature/reset password (#637)
    * Feature/password reset (#622)
    
    * feature(authentication): password recovery views
    
    * feature(authentication): login language switch, i18n translations
    
    * feature(authentication): connect API with forms
    
    * Cr fix
    
    * bugfix(authentication): change api property name
    
    * bugfix(ui): prevent key event on textField
    
    * bugfix(authentication): fix password validation

---
+++ modules/@ergonode/ui/src/components/Form/FormHeader.vue
@@ -0,0 +1,28 @@
+/*
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE for license details.
+ */
+<template>
+    <h2
+        class="form-header"
+        v-text="title" />
+</template>
+
+<script>
+export default {
+    name: 'FormHeader',
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+    .form-header {
+        color: $GRAPHITE_DARK;
+        font: $FONT_SEMI_BOLD_20_24;
+    }
+</style>
