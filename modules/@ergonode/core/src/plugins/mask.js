9f519fc5cac136606addd55f29fa223c88356c02
Piotr Bletek <p.bletek@gmail.com>
2021-02-01 15:36:47 +0100

    chore: add v-mask library and install plugin

---
+++ modules/@ergonode/core/src/plugins/mask.js
@@ -0,0 +1,10 @@
+/*
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE for license details.
+ */
+import {
+    VueMaskDirective,
+} from 'v-mask';
+import Vue from 'vue';
+
+Vue.directive('mask', VueMaskDirective);
