a2a05628e73145e580f261ffe1ee3ca7e40aa4b3
Piotr Bletek <p.bletek@gmail.com>
2021-03-03 10:19:43 +0100

    refactor: clear media relations

--- modules/@ergonode/media/src/config/extends.js
+++ modules/@ergonode/media/src/config/extends.js
@@ -5,12 +5,73 @@
 
 import {
     Components,
+    Store,
 } from '@Media/config/imports';
+import {
+    ATTRIBUTE_TYPES,
+} from '@Media/defaults';
+import {
+    getImage,
+    setImage,
+} from '@Media/extends/productTemplate/methods';
 
 export default {
+    extendStore: {
+        productTemplate: Store.Template,
+    },
     extendComponents: {
         '@UI/components/Grid/Layout/Collection/Cells': {
             MEDIA_ATTACH: Components.GridMediaAttachCollectionCell,
+        },
+        '@UI/components/Grid/Layout/Table/Cells/Edit': {
+            [ATTRIBUTE_TYPES.IMAGE]: Components.GridImageEditCell,
+            [ATTRIBUTE_TYPES.GALLERY]: Components.GridGalleryEditCell,
+            [ATTRIBUTE_TYPES.FILE]: Components.GridFileEditCell,
+        },
+        '@Templates/components/Forms/ProductTemplateForm': {
+            __ALL: [
+                {
+                    component: Components.ProductTemplateFormUploadImage,
+                    order: 10,
+                },
+            ],
+        },
+        '@Products/components/Forms/ProductTemplateForm': {
+            [ATTRIBUTE_TYPES.IMAGE]: [
+                {
+                    component: Components.ProductTemplateFormImageField,
+                },
+            ],
+            [ATTRIBUTE_TYPES.GALLERY]: [
+                {
+                    component: Components.ProductTemplateFormGalleryField,
+                },
+            ],
+            [ATTRIBUTE_TYPES.FILE]: [
+                {
+                    component: Components.ProductTemplateFormFileField,
+                },
+            ],
+        },
+    },
+    extendMethods: {
+        '@Templates/store/productTemplate/action/createTemplate/__before': ({
+            $this,
+        }) => getImage({
+            $this,
+        }),
+        '@Templates/store/productTemplate/action/updateTemplate/__before': ({
+            $this,
+        }) => getImage({
+            $this,
+        }),
+        '@Templates/store/productTemplate/action/getTemplate/__after': ({
+            $this, data,
+        }) => {
+            setImage({
+                $this,
+                data,
+            });
         },
     },
 };
---
+++ modules/@ergonode/media/src/extends/product/components/Forms/Field/ProductTemplateFormField.vue
@@ -0,0 +1,55 @@
+/*
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE for license details.
+ */
+<template>
+    <div
+        :class="classes"
+        :style="formFieldPosition">
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductTemplateFormField',
+    props: {
+        position: {
+            type: Object,
+            required: true,
+        },
+        size: {
+            type: Object,
+            required: true,
+        },
+    },
+    computed: {
+        classes() {
+            return [
+                'template-form-field',
+            ];
+        },
+        formFieldPosition() {
+            const {
+                x, y,
+            } = this.position;
+            const {
+                width, height,
+            } = this.size;
+            const offset = 1;
+            const fixedRow = y + offset;
+            const fixedColumn = x + offset;
+
+            return {
+                gridArea: `${fixedRow} / ${fixedColumn} / ${fixedRow + height} / ${fixedColumn + width}`,
+            };
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+    .template-form-field {
+        display: flex;
+    }
+</style>
