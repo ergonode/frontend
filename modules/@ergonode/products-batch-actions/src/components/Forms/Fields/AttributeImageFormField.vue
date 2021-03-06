e54fd34c23b697abb503935058e09d94a850563c
Piotr Bletek <p.bletek@gmail.com>
2021-03-06 17:53:47 +0100

    feature(batch-actions): add support for madia attributes (#751)

---
+++ modules/@ergonode/products-batch-actions/src/components/Forms/Fields/AttributeImageFormField.vue
@@ -0,0 +1,68 @@
+/*
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE for license details.
+ */
+<template>
+    <UploadImageFile
+        :value="value"
+        height="216px"
+        :label="label"
+        :error-messages="errorMessages"
+        @focus="onFocus"
+        @input="onValueChange" />
+</template>
+
+<script>
+import UploadImageFile from '@Media/components/Inputs/UploadFile/UploadImageFile';
+
+export default {
+    name: 'AttributeImageFormField',
+    components: {
+        UploadImageFile,
+    },
+    props: {
+        value: {
+            type: String,
+            default: '',
+        },
+        attribute: {
+            type: Object,
+            default: () => ({}),
+        },
+        languageCode: {
+            type: String,
+            required: true,
+        },
+        errorMessages: {
+            type: String,
+            default: '',
+        },
+    },
+    computed: {
+        label() {
+            return this.attribute.label[this.languageCode] || `#${this.attribute.code}`;
+        },
+    },
+    mounted() {
+        this.onValueChange('');
+    },
+    methods: {
+        onValueChange(value) {
+            this.$emit('input', {
+                key: this.attribute.id,
+                value,
+                languageCode: this.languageCode,
+            });
+        },
+        onFocus(isFocused) {
+            if (!isFocused) {
+                this.$emit('blur', {
+                    key: this.attribute.id,
+                    value: this.value,
+                    languageCode: this.languageCode,
+                });
+            }
+        },
+    },
+};
+</script>
