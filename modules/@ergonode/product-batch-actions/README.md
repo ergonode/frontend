13c4ca78fcfed8ec26285844a154e55f09e02c4c
Piotr Bletek <p.bletek@gmail.com>
2021-03-08 15:20:08 +0100

    hotfix: change module name

---
+++ modules/@ergonode/product-batch-actions/README.md
@@ -0,0 +1,16 @@
+
+<p align="center">
+  <a href="https://ergonode.com" rel="noopener noreferrer">
+    <img width="300" src="https://ergonode.com/img/logo-dark.svg" alt="Ergonode logo">
+  </a>
+</p>
+<h3 align="center">Product batch actions module</h3>
+<p align="center">
+  <a href="https://www.npmjs.com/package/@ergonode/product-batch-actions">
+    <img alt="npm" src="https://img.shields.io/npm/v/@ergonode/product-batch-actions">
+  </a>
+  <a href="https://www.npmjs.com/package/@ergonode/product-batch-actions">
+    <img alt="npm" src="https://img.shields.io/npm/l/@ergonode/product-batch-actions">
+  </a>
+</p>
+<br>
---
+++ modules/@ergonode/product-batch-actions/src/components/NotificationLists/Items/NotificationListBatchActionEndedItem.vue
@@ -0,0 +1,51 @@
+/*
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE for license details.
+ */
+<template>
+    <NotificationListItemTemplate
+        :item="item"
+        @details="onShowDetails">
+        <template #prepend>
+            <NotificationListItemAvatar :item="item" />
+            <ProductBatchActionDetailsModalGrid
+                v-if="isModalVisible"
+                :item="item"
+                @close="onHideDetails" />
+        </template>
+    </NotificationListItemTemplate>
+</template>
+
+<script>
+
+import NotificationListItemAvatar from '@Notifications/components/NotificationList/Item/NotificationListItemAvatar';
+import NotificationListItemTemplate from '@Notifications/components/NotificationList/Item/NotificationListItemTemplate';
+
+export default {
+    name: 'NotificationListBatchActionEndedItem',
+    components: {
+        NotificationListItemAvatar,
+        NotificationListItemTemplate,
+        ProductBatchActionDetailsModalGrid: () => import('@ProductBatchActions/components/Modals/ProductBatchActionDetailsModalGrid'),
+    },
+    props: {
+        item: {
+            type: Object,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            isModalVisible: false,
+        };
+    },
+    methods: {
+        onShowDetails() {
+            this.isModalVisible = true;
+        },
+        onHideDetails() {
+            this.isModalVisible = false;
+        },
+    },
+};
+</script>
