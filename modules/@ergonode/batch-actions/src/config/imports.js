83114a30ec3150bef37d0037ceed6568ea461fa3
Piotr Bletek <p.bletek@gmail.com>
2021-03-16 18:05:44 +0100

    Feature/action center with batch actions (#772)
    * feature(batch-actions): add to action center
    
    * bugfix(batch-action): remove async mathods

---
+++ modules/@ergonode/batch-actions/src/config/imports.js
@@ -0,0 +1,10 @@
+/*
+ * Copyright © Ergonode Sp. z o.o. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+export const Components = {
+    NotificationListBatchActionsSuccessItem: () => import('@BatchActions/extends/notification/components/Notifications/NotificationListBatchActionsSuccessItem').then(m => m.default || m),
+    NotificationListBatchActionsWarningItem: () => import('@BatchActions/extends/notification/components/Notifications/NotificationListBatchActionsWarningItem').then(m => m.default || m),
+    NotificationListBatchActionsProcessingItem: () => import('@BatchActions/extends/notification/components/Notifications/NotificationListBatchActionsProcessingItem').then(m => m.default || m),
+};
---
+++ modules/@ergonode/batch-actions/src/extends/notification/components/Notifications/NotificationListBatchActionsWarningItem.vue
@@ -0,0 +1,31 @@
+/*
+ * Copyright © Ergonode Sp. z o.o. All rights reserved.
+ * See LICENSE for license details.
+ */
+<template>
+    <NotificationListItemTemplate :item="item">
+        <template #prepend>
+            <NotificationListItemWarning :item="item" />
+        </template>
+    </NotificationListItemTemplate>
+</template>
+
+<script>
+
+import NotificationListItemTemplate from '@Notifications/components/NotificationList/Item/NotificationListItemTemplate';
+import NotificationListItemWarning from '@Notifications/components/NotificationList/Item/NotificationListItemWarning';
+
+export default {
+    name: 'NotificationListBatchActionsWarningItem',
+    components: {
+        NotificationListItemTemplate,
+        NotificationListItemWarning,
+    },
+    props: {
+        item: {
+            type: Object,
+            required: true,
+        },
+    },
+};
+</script>
