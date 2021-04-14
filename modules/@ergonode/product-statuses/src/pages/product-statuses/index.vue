dfa09fc24b46a725094fa305aaab43de172a4e57
Maciej Kaczorowski <46050724+derpdead@users.noreply.github.com>
2021-04-14 13:04:00 +0200

    Product statuses interface translations (#832)
    * feat(core): product statuses translations
    
    * feat(core): polish translation

--- modules/@ergonode/product-statuses/src/pages/product-statuses/index.vue
+++ modules/@ergonode/product-statuses/src/pages/product-statuses/index.vue
@@ -5,7 +5,7 @@
 <template>
     <Page>
         <TitleBar
-            title="Product statuses"
+            :title="$t('@Statuses.productStatus._.title')"
             :is-read-only="isReadOnly">
             <template #mainAction>
                 <template
@@ -65,7 +65,7 @@ export default {
     },
     head() {
         return {
-            title: 'Product statuses - Ergonode',
+            title: this.$t('@Statuses.productStatus._.headTitle'),
         };
     },
 };
