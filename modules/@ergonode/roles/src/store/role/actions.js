9f38589a42bf02ee24d17919b392ac4e74d930e5
Piotr Bletek <p.bletek@gmail.com>
2021-04-26 13:50:03 +0200

    bugfix: role default update property (#856)
    * bugfix: role default update property

--- modules/@ergonode/roles/src/store/role/actions.js
+++ modules/@ergonode/roles/src/store/role/actions.js
@@ -86,7 +86,7 @@ export default {
         },
         {
             scope,
-            drafts,
+            drafts = {},
             onSuccess = () => {},
             onError = () => {},
         },
