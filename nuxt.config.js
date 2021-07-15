6e2012c44eb49704c506da2d156a39dd9dd0a478
Piotr Bletek <p.bletek@gmail.com>
2021-07-15 08:53:16 +0200

    e2e job fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -149,8 +149,6 @@ module.exports = {
             configFile: './babel.config.js',
         },
         parallel: PARALLEL,
-        cache: true,
-        hardSource: true,
         cssSourceMap: true,
         optimizeCSS: true,
         loaders: {
@@ -201,19 +199,6 @@ module.exports = {
             splitChunks: {
                 chunks: 'all',
                 maxSize: 200000,
-            },
-        },
-        html: {
-            minify: {
-                collapseBooleanAttributes: true,
-                decodeEntities: true,
-                minifyCSS: false,
-                minifyJS: false,
-                processConditionalComments: true,
-                removeEmptyAttributes: true,
-                removeRedundantAttributes: true,
-                trimCustomFragments: true,
-                useShortDoctype: true,
             },
         },
     },
