504254219c7bc088e047ce84a8dfcc2158965920
Piotr Bletek <p.bletek@gmail.com>
2021-07-15 07:35:06 +0200

    cypress ci run performance

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,6 +2,36 @@
  * Copyright © Ergonode Sp. z o.o. All rights reserved.
  * See LICENSE for license details.
  */
+
+const commands = [];
+
+Cypress.on('test:after:run', (attributes) => {
+    /* eslint-disable no-console */
+    console.log('Test "%s" has finished in %dms',
+        attributes.title, attributes.duration);
+    console.table(commands);
+    commands.length = 0;
+});
+
+Cypress.on('command:start', (c) => {
+    commands.push({
+        name: c.attributes.name,
+        args: c.attributes.args.toString(),
+        started: +new Date(),
+    });
+});
+
+Cypress.on('command:end', (c) => {
+    const lastCommand = commands[commands.length - 1];
+
+    if (lastCommand.name !== c.attributes.name) {
+        throw new Error('Last command is wrong');
+    }
+
+    lastCommand.endedAt = +new Date();
+    lastCommand.elapsed = `${lastCommand.endedAt - lastCommand.started} ms`;
+});
+
 Cypress.Commands.add('login', (email, pass) => {
     cy.getCookie('token')
         .then((token) => {
