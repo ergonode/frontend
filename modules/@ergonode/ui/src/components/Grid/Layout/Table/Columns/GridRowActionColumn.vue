71cb7bbc34f2d1ec11076ef3896598e02c9261da
Maciej Kaczorowski <maciej.kaczorowski@strix.net>
2021-09-01 09:58:23 +0200

    feat(core): clean code

--- modules/@ergonode/ui/src/components/Grid/Layout/Table/Columns/GridRowActionColumn.vue
+++ modules/@ergonode/ui/src/components/Grid/Layout/Table/Columns/GridRowActionColumn.vue
@@ -131,11 +131,6 @@ export default {
             return this.isBasicFilter ? 1 : 0;
         },
     },
-    watch: {
-        actionCellComponents() {
-            console.log('set', this.actionCellComponents);
-        },
-    },
     methods: {
         onRowAction(payload) {
             this.$emit('row-action', payload);
