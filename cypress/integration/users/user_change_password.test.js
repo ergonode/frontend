b439214c05c550c34d2538e8da9b4cfb24cbb429
Piotr Kreft <kreftpiotrek@gmail.com>
2021-09-08 10:40:46 +0200

    docs: Updated copyrights

--- cypress/integration/users/user_change_password.test.js
+++ cypress/integration/users/user_change_password.test.js
@@ -1,6 +1,6 @@
 /* eslint-disable jest/valid-expect-in-promise */
 /*
- * Copyright © Ergonode Sp. z o.o. All rights reserved.
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
  * See LICENSE for license details.
  */
 context('Change User password', () => {
