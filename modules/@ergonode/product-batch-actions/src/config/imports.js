9ea72a11b4dc2e53c4b7fb1537078cc821937a53
Maciej Kaczorowski <46050724+derpdead@users.noreply.github.com>
2021-09-10 09:58:11 +0200

    feat(core): add extending batch action endpoint (#1058)

--- modules/@ergonode/product-batch-actions/src/config/imports.js
+++ modules/@ergonode/product-batch-actions/src/config/imports.js
@@ -6,6 +6,7 @@
 export const Components = {
     ProductsBatchActionsButton: () => import('@ProductBatchActions/components/Buttons/ProductsBatchActionsButton').then(m => m.default || m),
     NotificationListBatchActionEndedItem: () => import('@ProductBatchActions/components/NotificationLists/Items/NotificationListBatchActionEndedItem').then(m => m.default || m),
+    NotificationListBatchActionsProcessingItem: () => import('@ProductBatchActions/components/NotificationLists/Items/NotificationListBatchActionsProcessingItem').then(m => m.default || m),
     AttributesVerticalTab: () => import('@ProductBatchActions/components/VerticalTabs/AttributesVerticalTab'),
 };
 
