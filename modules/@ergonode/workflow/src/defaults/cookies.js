1f33bf097102c611e6f079d1b7123b3be0df460d
Piotr Kreft <kreftpiotrek@gmail.com>
2021-12-29 10:13:37 +0100

    docs: Updated copyrights

--- modules/@ergonode/workflow/src/defaults/cookies.js
+++ modules/@ergonode/workflow/src/defaults/cookies.js
@@ -1,5 +1,5 @@
 /*
- * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * Copyright © Ergonode Sp. z o.o. All rights reserved.
  * See LICENSE for license details.
  */
 export const WORKFLOW_DESIGNER_ROWS = 'WORKFLOW_DESIGNER_ROWS';
