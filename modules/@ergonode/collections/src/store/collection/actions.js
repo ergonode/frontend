6b4f9793bce4c94dbab724e2a17acc46a96b3a63
Piotr Kreft <49146985+piotrkreft@users.noreply.github.com>
2021-12-29 10:31:43 +0100

    docs: Updated copyrights (#1177)

--- modules/@ergonode/collections/src/store/collection/actions.js
+++ modules/@ergonode/collections/src/store/collection/actions.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-throw-literal */
 /*
- * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * Copyright © Ergonode Sp. z o.o. All rights reserved.
  * See LICENSE for license details.
  */
 import {
