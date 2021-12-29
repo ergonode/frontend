83aaf8023f8b46f9cee034b1fbff4fce5157265d
Piotr Kreft <49146985+piotrkreft@users.noreply.github.com>
2021-12-29 10:31:33 +0100

    docs: Updated copyrights (#1176)

--- modules/@ergonode/core/src/defaults/alerts/index.js
+++ modules/@ergonode/core/src/defaults/alerts/index.js
@@ -1,5 +1,5 @@
 /*
- * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * Copyright © Ergonode Sp. z o.o. All rights reserved.
  * See LICENSE for license details.
  */
 export const ALERT_TIME_TO_LEAVE = 3000;
